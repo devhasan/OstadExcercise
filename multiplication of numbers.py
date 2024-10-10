d3ae361b5276f501169268f54144da0e4ea0e8c0
Hasan Mahmud <hasanmpro@gmail.com>
2024-10-10 23:34:28 +0600

    edited

--- multiplication of numbers.py
+++ multiplication of numbers.py
@@ -1,10 +1,8 @@
-"""
+
 def multiplication(num):
+    print(type(num))
     for i in range(10):
         print(num, "X", i+1, "=", num*(i+1))
 
-number = int(input("Enter a number: "))
+number = float(input("Enter a number: "))
 multiplication(number)
-"""
-
-25/0
