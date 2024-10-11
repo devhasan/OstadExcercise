a0864a30892ac352d924a42d5e12d984227778cb
Hasan Mahmud <hasanmpro@gmail.com>
2024-10-12 00:33:42 +0600

    comment on the file

--- multiplication of numbers.py
+++ multiplication of numbers.py
@@ -1,8 +1,7 @@
+# Multiplication calculator useing function
 
 def multiplication(num):
-    print(type(num))
     for i in range(10):
         print(num, "X", i+1, "=", num*(i+1))
-
 number = float(input("Enter a number: "))
 multiplication(number)
